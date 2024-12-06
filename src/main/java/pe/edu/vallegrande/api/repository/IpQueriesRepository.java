c3ff743fdc8b4a955dcc9c8df145e55b4f2b4b96
AngelCastillaSandoval <angel.castilla@vallegrande.edu.pe>
2024-12-06 16:30:30 -0500

    back

--- src/main/java/pe/edu/vallegrande/api/repository/IpQueriesRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.vallegrande.api.repository;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Flux;
-import pe.edu.vallegrande.api.model.IpQueriesModel;
-
-public interface IpQueriesRepository extends ReactiveCrudRepository<IpQueriesModel, Long> {
-    Flux<IpQueriesModel> findByStatus(String status);
-}
